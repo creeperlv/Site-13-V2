80b0cbbe87eb50d4b35078a47e43a732694be996
Creeper Lv <creeperlv38@outlook.com>
2021-08-15 01:27:47 +0800

    [0.2.90.0] -Working on a simple ECS.

---
+++ Tools/CommonTools/Site13Kernel/Core/SystemBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Site13Kernel.Core
+{
+    public class SystemBase
+    {
+        public virtual void Execute()
+        {
+        }
+    }
+    public interface IComponent
+    {
+    
+    }
+}
---
+++ Tools/CommonTools/Site13Kernel/Utilities/RandomTool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Site13Kernel.Utilities
+{
+    public class RandomTool
+    {
+        static Random random;
+        public static void Init()
+        {
+            random = new Random();
+        }
+        public static void Init(int Seed)
+        {
+            random = new Random(Seed);
+        }
+        public static int NextInt()
+        {
+            return random.Next();
+        }
+        public static int NextInt(int Upper)
+        {
+            return random.Next(0, Upper);
+        }
+        public static int NextInt(int Lower, int Upper) => random.Next(Lower, Upper);
+    }
+}
