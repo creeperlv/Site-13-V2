5dca2bc462c53967d7b527c471b6d801e618b5ac
Creeper Lv <creeperlv38@outlook.com>
2021-09-02 01:37:52 +0800

    [0.2.90.0] -Working on OOBE.

---
+++ Site-13-V2/Assets/SCPAssets/Codes/UI/SP/OOBEMain.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Site13Kernel.UI.SP
+{
+    public class OOBEMain : MonoBehaviour
+    {
+        public Dropdown LanguageSelect;
+        void Start()
+        {
+        
+        }
+
+    }
+}
