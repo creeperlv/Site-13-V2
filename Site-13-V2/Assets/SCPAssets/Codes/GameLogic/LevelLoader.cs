eca026da001ff6f47da79605ae3c05bdca67323d
Creeper Lv <creeperlv38@outlook.com>
2021-09-07 00:34:26 +0800

    [0.2.90.0] -Working on levelloader.

---
+++ Site-13-V2/Assets/SCPAssets/Codes/GameLogic/LevelLoader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Site13Kernel.GameLogic
+{
+    public class LevelLoader : MonoBehaviour
+    {
+
+        void Start()
+        {
+        
+        }
+
+    }
+}
