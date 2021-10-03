4fddc5a838f052f93a8f5d9c0d3339c935c191be
Creeper Lv <creeperlv@users.noreply.github.com>
2021-10-04 02:40:46 +0800

    [0.2.90.0] -Working on Weapon, UI...

--- Site-13-V2/Assets/SCPAssets/Codes/UI/Combat/WeaponHUD.cs
+++ Site-13-V2/Assets/SCPAssets/Codes/UI/Combat/WeaponHUD.cs
@@ -1,4 +1,5 @@
 using Site13Kernel.Core;
+using Site13Kernel.GameLogic.FPS;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,13 @@ namespace Site13Kernel.UI.Combat
     public class WeaponHUD : ControlledBehavior
     {
         public Text DisplayText;
+        public ProgressBar ProgressBar;
         public bool isPercentage;
+        public bool isPrimary;
+        public BasicWeapon ListeningWeapon;
+        public override void Refresh(float DeltaTime, float UnscaledDeltaTime)
+        {
+
+        }
     }
 }
---
+++ Site-13-V2/Assets/SCPAssets/Codes/UI/ProgressBar.cs
@@ -0,0 +1,120 @@
+using Site13Kernel.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Site13Kernel.UI
+{
+    public class ProgressBar : ControlledBehavior, IPropertiedObject, IContentable
+    {
+        float V;
+        public float Value
+        {
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => V;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                V = value;
+                OnValueUpdate();
+            }
+        }
+        float MinV;
+        float MaxV;
+        public float MinValue
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => MinV;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                MinV = value;
+                OnValueUpdate();
+            }
+        }
+        public float MaxValue
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => MaxV;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                MaxV = value;
+                OnValueUpdate();
+            }
+        }
+        public Image FillImage;
+        public Text ProgressText;
+        public bool isTextEnabled;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void OnValueUpdate()
+        {
+
+            FillImage.fillAmount = Mathf.InverseLerp(MinValue, MaxValue, V);
+        }
+        public string Content
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Value.ToString();
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => Value = float.Parse(value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Property GetProperty(string name)
+        {
+            var v = GetPropertyValue(name);
+            if (v == null)
+                return null;
+            else
+                return new Property { Key = name, Value = v };
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public object GetPropertyValue(string name)
+        {
+            switch (name)
+            {
+                case "Value":
+                    return Value;
+                case "MaxValue":
+                    return MaxV;
+                case "MinValue":
+                    return MinV;
+                default:
+                    break;
+            }
+            return null;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetProperty(string name, object value)
+        {
+            switch (name)
+            {
+                case "Value":
+                    Value = (float)value;
+                    break;
+                case "MaxValue":
+                    MaxValue = (float)value;
+                    break;
+                case "MinValue":
+                    MinValue = (float)value;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetProperty(Property p)
+        {
+            SetProperty(p.Key, p.Value);
+        }
+    }
+}
