3f40e72f8f0027ed6471a4a1d4562bf709923762
Creeper Lv <creeperlv@users.noreply.github.com>
2021-10-22 01:11:09 +0800

    [0.2.90.0] -Working on the ChunkDifferentialFrameSyncController. -Working on Pickups.

--- Tools/CommonTools/Site13Kernel.UnityEngine/Core/Controllers/ChunkDifferentialFrameSyncController.cs
+++ Tools/CommonTools/Site13Kernel.UnityEngine/Core/Controllers/ChunkDifferentialFrameSyncController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Site13Kernel.Diagnostics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
@@ -7,33 +8,102 @@ namespace Site13Kernel.Core.Controllers
 {
     public class ChunkDifferentialFrameSyncController : BehaviorController
     {
+        public List<ChunkDifferentialFrameSyncSubController> subControllers;
+        int FRAME_UPDATE = 0;
+        int FRAME_FIXEDUPDATE = 0;
         void Update()
         {
             float DeltaTime = Time.deltaTime;
             float UDeltaTime = Time.unscaledDeltaTime;
-            foreach (var item in _OnRefresh)
+            foreach (var item in subControllers)
             {
                 if (INTERRUPT_REFRESH) continue;
-                item.Refresh(DeltaTime, UDeltaTime);
+                item.Refresh(DeltaTime, UDeltaTime, FRAME_UPDATE % subControllers.Count);
+            }
+            if (FRAME_UPDATE % subControllers.Count == 0 && FRAME_UPDATE != 0)
+            {
+                FRAME_UPDATE = 1;
+            }
+            else
+            {
+                FRAME_UPDATE++;
+            }
+        }
+        void FixedUpdate()
+        {
+            float DeltaTime = Time.fixedDeltaTime;
+            float UDeltaTime = Time.fixedUnscaledDeltaTime;
+            foreach (var item in subControllers)
+            {
+                if (INTERRUPT_REFRESH) continue;
+                item.FixedRefresh(DeltaTime, UDeltaTime, FRAME_FIXEDUPDATE % subControllers.Count);
+            }
+            if (FRAME_FIXEDUPDATE % subControllers.Count == 0 && FRAME_FIXEDUPDATE != 0)
+            {
+                FRAME_FIXEDUPDATE = 1;
+            }
+            else
+            {
+                FRAME_FIXEDUPDATE++;
             }
         }
     }
-    public class ChunkDifferentialFrameSyncSubController : BehaviorController, IControllable
+    public class ChunkDifferentialFrameSyncSubController : BehaviorController
     {
-        public int FRAME_IGNORANCE;
+        public int ID;
         public BaseController Parent { get; set; }
-
-        public void FixedRefresh(float DeltaTime, float UnscaledDeltaTime)
+        float FR_DT = 0;
+        float FR_UDT = 0;
+        float R_DT = 0;
+        float R_UDT = 0;
+        public void FixedRefresh(float DeltaTime, float UnscaledDeltaTime, int CURRENTFRAME)
         {
+            FR_DT += DeltaTime;
+            FR_UDT += UnscaledDeltaTime;
+            if (CURRENTFRAME == ID)
+            {
+                foreach (var item in _OnFixedRefresh)
+                {
+                    try
+                    {
+                        item.FixedRefresh(FR_DT, FR_UDT);
+
+                    }
+                    catch (Exception e)
+                    {
+                        Debugger.CurrentDebugger.LogError(e);
+                    }
+                }
+            }
         }
 
         public void Init()
         {
-
+            SerializeAll();
+            foreach (var item in _OnInit)
+            {
+                item.Init();
+            }
         }
-        public void Refresh(float DeltaTime, float UnscaledDeltaTime)
+        public void Refresh(float DeltaTime, float UnscaledDeltaTime, int CURRENTFRAME)
         {
+            R_DT += DeltaTime;
+            R_UDT += UnscaledDeltaTime;
+            if (CURRENTFRAME == ID)
+            {
+                foreach (var item in _OnRefresh)
+                {
+                    try
+                    {
+                        item.Refresh(R_DT, R_UDT);
 
+                    }
+                    catch (Exception e)
+                    {
+                        Debugger.CurrentDebugger.LogError(e);
+                    }
+                }
+            }
         }
     }
 }
---
+++ Tools/CommonTools/Site13Kernel.UnityEngine/GameLogic/FPS/Pickupable.cs
@@ -0,0 +1,18 @@
+﻿using Site13Kernel.Core.Interactives;
+using Site13Kernel.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Site13Kernel.GameLogic.FPS
+{
+    public class Pickupable : InteractiveBase
+    {
+        public PickupItem ItemType;
+        public Weapon Weapon;
+    }
+    public enum PickupItem
+    {
+        Weapon, Grenade
+    }
+}
