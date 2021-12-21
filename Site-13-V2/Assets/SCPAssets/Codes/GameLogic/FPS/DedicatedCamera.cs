5e72dde0f2699f5e19712f9b64e663202ec23ed7
Creeper Lv <creeperlv@users.noreply.github.com>
2021-12-22 03:56:28 +0800

    [0.2.90.0]
    -Added a dedicated camera to perform a locked angle cam.

---
+++ Site-13-V2/Assets/SCPAssets/Codes/GameLogic/FPS/DedicatedCamera.cs
@@ -0,0 +1,46 @@
+using Site13Kernel.Core;
+using Site13Kernel.Core.CustomizedInput;
+using Site13Kernel.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Site13Kernel.GameLogic
+{
+    public class DedicatedCamera : ControlledBehavior
+    {
+        public Transform TargetCam;
+        public float MinH;
+        public float MaxH;
+        public float MinV;
+        public float MaxV;
+        public float IntensityH = 1;
+        public float IntensityV = 1;
+        public override void Init()
+        {
+            Parent.RegisterRefresh(this);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override void Refresh(float DeltaTime, float UnscaledDeltaTime)
+        {
+            var H = InputProcessor.CurrentInput.GetAxis("MouseH");
+            var V = InputProcessor.CurrentInput.GetAxis("MouseV");
+            var EA = TargetCam.localRotation.eulerAngles;
+            var __H = Mathf.Clamp((EA.y > 180 ? EA.y - 360 : EA.y) + H * IntensityH, MinH, MaxH);
+            if (__H < 0)
+            {
+                __H += 360;
+            }
+            var __V = Mathf.Clamp((EA.x > 180 ? EA.x - 360 : EA.x) + V * IntensityV, MinV, MaxV);
+            if (__V < 0)
+            {
+                __V += 360;
+            }
+            EA.y = __H;
+            EA.x = __V;
+            TargetCam.localRotation = Quaternion.Euler(EA);
+
+        }
+    }
+}
