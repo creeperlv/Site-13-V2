710dcd97c14b24cabf12579925fb2e86f5c2ea34
Creeper Lv <creeperlv@users.noreply.github.com>
2021-12-23 04:56:53 +0800

    [0.2.90.0]
    -Working on FixedDirector.

--- Site-13-V2/Assets/SCPAssets/Codes/GameLogic/CampaignScripts/FixedDirector.cs
+++ Site-13-V2/Assets/SCPAssets/Codes/GameLogic/CampaignScripts/FixedDirector.cs
@@ -1,6 +1,5 @@
 using Site13Kernel.Core.Controllers;
 using Site13Kernel.Diagnostics;
-using Site13Kernel.GameLogic.AI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -61,7 +60,7 @@ namespace Site13Kernel.GameLogic.CampaignScripts
                             try
                             {
 
-                                var PLAYER = GlobalBioController.CurrentGlobalBioController.Spawn("PLAYER", Vector3.zero, Vector3.zero);
+                                var PLAYER = GlobalBioController.CurrentGlobalBioController.Spawn(e.PlayerSpawnID, Vector3.zero, Vector3.zero);
                                 PLAYER.transform.GetChild(1).position = e.Location.position;
                                 PLAYER.transform.GetChild(1).rotation = e.Location.rotation;
                                 FPSC = PLAYER.GetComponentInChildren<FPSController>();
@@ -102,6 +101,15 @@ namespace Site13Kernel.GameLogic.CampaignScripts
                             else
                             {
                                 e.ControlledObject.SetActive(e.TargetState);
+                            }
+                        }
+                        break;
+                    case EventType.GivePlayerWeapon:
+                        {
+                            if(FPSC != null)
+                            {
+                                var BAG = FPSC.BagHolder;
+                                //TODO
                             }
                         }
                         break;
@@ -163,41 +171,9 @@ namespace Site13Kernel.GameLogic.CampaignScripts
             }
         }
     }
-    [Serializable]
-    public class CampaignEvent
-    {
-        public bool isIgnored;
-        public bool useTrigger;
-        public bool useTimer;
-        public bool AllowDuplicateExecution;
-        public bool Executed;
-        public EventTrigger Trigger;
-        public float OnTriggeredTime;
-        public EventType TriggerType;
-        [Header("Speak")]
-        public AudioSource Audio;
-        public string Subtitle;
-        [Header("Spawn")]
-        public string SpawnID;
-        public Vector3 DeltaPosition;
-        public Routine Routine;
-        public AIState InitState;
-        [Header("TriggerGameObject")]
-        public GameObject ControlledObject;
-        public bool isReverse;
-        public bool TargetState;
-        [Header("General")]
-        public Transform Location;
-        [Header("Script")]
-        public string ExecutingScript;
-        #region ExecutorFields
-        [HideInInspector]
-        public float TimeD;
-        #endregion
-    }
 
     public enum EventType
     {
-        Speak, Spawn, Win, Script, SpawnPlayer, TriggerGameObject, DisablePlayer, EnablePlayer
+        Speak, Spawn, Win, Script, SpawnPlayer, TriggerGameObject, DisablePlayer, EnablePlayer,GivePlayerWeapon
     }
 }
