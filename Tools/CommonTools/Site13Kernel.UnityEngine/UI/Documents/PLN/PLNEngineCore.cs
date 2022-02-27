12693e87bf90e6f4e333a1ba6a0b66fc4df721ae
Creeper Lv <creeperlv@users.noreply.github.com>
2022-02-27 19:28:51 +0800

    [0.2.90.0] -Updated CLUNL libraries.

--- Tools/CommonTools/Site13Kernel.UnityEngine/UI/Documents/PLN/PLNEngineCore.cs
+++ Tools/CommonTools/Site13Kernel.UnityEngine/UI/Documents/PLN/PLNEngineCore.cs
@@ -32,219 +32,220 @@ namespace Site13Kernel.UI.Documents.PLN
 
             foreach (var item in contents)
             {
-                Process(item);
+                Process(item, Container, DefaultColor, BaseSize);
             }
-
-            void Process(string item)
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void SequentialProcessLine(string Line, Text t, int BaseSize)
+        {
+            if (Line.StartsWith("[C]"))
             {
-                if (item.StartsWith("[Img]"))
+                var item = Line.Substring(3).Trim();
+                t.alignment = TextAnchor.MiddleCenter;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[R]"))
+            {
+                var item = Line.Substring(3).Trim();
+                t.alignment = TextAnchor.MiddleRight;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[L]"))
+            {
+                var item = Line.Substring(3).Trim();
+                t.alignment = TextAnchor.MiddleRight;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[B]"))
+            {
+                var item = Line.Substring(3).Trim();
+                t.fontStyle = FontStyle.Bold;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[I]"))
+            {
+                var item = Line.Substring(3).Trim();
+                t.fontStyle = FontStyle.Italic;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[IB]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontStyle = FontStyle.BoldAndItalic;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[H1]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize + 2;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[H2]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize + 4;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[H3]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize + 6;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[H4]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize + 8;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[H5]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize + 10;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[H4]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize + 8;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[N1]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize - 2;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[N2]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize - 4;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[N3]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize - 6;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[N4]"))
+            {
+                var item = Line.Substring(4).Trim();
+                t.fontSize = BaseSize - 8;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[C/B]"))
+            {
+                var item = Line.Substring(5).Trim();
+                t.color = Color.black;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[C/W]"))
+            {
+                var item = Line.Substring(5).Trim();
+                t.color = Color.white;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[C/G]"))
+            {
+                var item = Line.Substring(5).Trim();
+                t.color = Color.green;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[C/R]"))
+            {
+                var item = Line.Substring(5).Trim();
+                t.color = Color.red;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[C/Y]"))
+            {
+                var item = Line.Substring(5).Trim();
+                t.color = Color.yellow;
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else if (Line.StartsWith("[C:"))
+            {
+                var item = Line.Substring(3).Trim();
+                var COLOR = item.Substring(0, 6);
+                if (ColorUtility.TryParseHtmlString(COLOR, out var c))
                 {
-                    var URL = item.Substring(5).Trim();
-                    Uri uri = new Uri(URL);
+                    t.color = c;
+                }
+                item = item.Substring(7).Trim();
+                SequentialProcessLine(item, t, BaseSize);
+            }
+            else
+            {
+                t.text = Line;
+            }
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void Process(string item, Transform Container, Color DefaultColor, int BaseSize)
+        {
+            if (item.StartsWith("[Img]"))
+            {
+                var URL = item.Substring(5).Trim();
+                Uri uri = new Uri(URL);
 
+                {
+                    var HOST = uri.GetComponents(UriComponents.Host, UriFormat.UriEscaped);
+                    var Name = uri.GetComponents(UriComponents.Path, UriFormat.UriEscaped);
+
+                    var o = GameObject.Instantiate(_ImageTemplate, Container);
+                    var t = o.GetComponent<Image>();
+                    var rt = o.transform as RectTransform;
+                    if (HOST.ToUpper() == "LOCAL.CREEPERLV.GITHUB.IO")
                     {
-                        var HOST = uri.GetComponents(UriComponents.Host, UriFormat.UriEscaped);
-                        var Name = uri.GetComponents(UriComponents.Path, UriFormat.UriEscaped);
-                        
-                        var o = GameObject.Instantiate(_ImageTemplate, Container);
-                        var t = o.GetComponent<Image>();
-                        var rt = o.transform as RectTransform;
-                        if (HOST.ToUpper() == "LOCAL.CREEPERLV.GITHUB.IO")
-                        {
 
-                            t.sprite = ImageStorage.FindSprite(Name).LoadedSprite;
-                        }
-                        var para = uri.GetComponents(UriComponents.Query, UriFormat.UriEscaped);
-                        if (para != null)
+                        t.sprite = ImageStorage.FindSprite(Name).LoadedSprite;
+                    }
+                    var para = uri.GetComponents(UriComponents.Query, UriFormat.UriEscaped);
+                    if (para != null)
+                    {
+                        var paras = para.Split('&');
+                        foreach (var p in paras)
                         {
-                            var paras = para.Split('&');
-                            foreach (var p in paras)
+                            var kv = p.Split('=');
+                            switch ((kv[0]).ToUpper())
                             {
-                                var kv = p.Split('=');
-                                switch ((kv[0]).ToUpper())
-                                {
-                                    case "W":
-                                    case "WIDTH":
-                                        {
-                                            float w = float.Parse(kv[1]);
-                                            var v = rt.sizeDelta;
-                                            v.x = w;
-                                            rt.sizeDelta = v;
-                                        }
-                                        break;
-                                    case "H":
-                                    case "HEIGHT":
-                                        {
-                                            float h = float.Parse(kv[1]);
-                                            var v = rt.sizeDelta;
-                                            v.y = h;
-                                            rt.sizeDelta = v;
-                                        }
-                                        break;
-                                    default:
-                                        break;
-                                }
+                                case "W":
+                                case "WIDTH":
+                                    {
+                                        float w = float.Parse(kv[1]);
+                                        var v = rt.sizeDelta;
+                                        v.x = w;
+                                        rt.sizeDelta = v;
+                                    }
+                                    break;
+                                case "H":
+                                case "HEIGHT":
+                                    {
+                                        float h = float.Parse(kv[1]);
+                                        var v = rt.sizeDelta;
+                                        v.y = h;
+                                        rt.sizeDelta = v;
+                                    }
+                                    break;
+                                default:
+                                    break;
                             }
                         }
                     }
                 }
-                else
-                {
-
-                    var t = GameObject.Instantiate(_TextTemplate, Container).GetComponent<Text>();
-                    if (t == null)
-                    {
-                        Debugger.CurrentDebugger.LogError("Text is null, is TextTemplate inited?");
-                        return;
-                    }
-                    t.fontSize = BaseSize;
-                    t.color = DefaultColor;
-                    SequentialProcessLine(item, t);
-                }
             }
-            void SequentialProcessLine(string Line, Text t)
+            else
             {
-                if (Line.StartsWith("[C]"))
+
+                var t = GameObject.Instantiate(_TextTemplate, Container).GetComponent<Text>();
+                if (t == null)
                 {
-                    var item = Line.Substring(3).Trim();
-                    t.alignment = TextAnchor.MiddleCenter;
-                    SequentialProcessLine(item, t);
+                    Debugger.CurrentDebugger.LogError("Text is null, is TextTemplate inited?");
+                    return;
                 }
-                else if (Line.StartsWith("[R]"))
-                {
-                    var item = Line.Substring(3).Trim();
-                    t.alignment = TextAnchor.MiddleRight;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[L]"))
-                {
-                    var item = Line.Substring(3).Trim();
-                    t.alignment = TextAnchor.MiddleRight;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[B]"))
-                {
-                    var item = Line.Substring(3).Trim();
-                    t.fontStyle = FontStyle.Bold;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[I]"))
-                {
-                    var item = Line.Substring(3).Trim();
-                    t.fontStyle = FontStyle.Italic;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[IB]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontStyle = FontStyle.BoldAndItalic;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[H1]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize + 2;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[H2]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize + 4;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[H3]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize + 6;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[H4]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize + 8;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[H5]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize + 10;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[H4]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize + 8;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[N1]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize - 2;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[N2]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize - 4;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[N3]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize - 6;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[N4]"))
-                {
-                    var item = Line.Substring(4).Trim();
-                    t.fontSize = BaseSize - 8;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[C/B]"))
-                {
-                    var item = Line.Substring(5).Trim();
-                    t.color = Color.black;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[C/W]"))
-                {
-                    var item = Line.Substring(5).Trim();
-                    t.color = Color.white;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[C/G]"))
-                {
-                    var item = Line.Substring(5).Trim();
-                    t.color = Color.green;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[C/R]"))
-                {
-                    var item = Line.Substring(5).Trim();
-                    t.color = Color.red;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[C/Y]"))
-                {
-                    var item = Line.Substring(5).Trim();
-                    t.color = Color.yellow;
-                    SequentialProcessLine(item, t);
-                }
-                else if (Line.StartsWith("[C:"))
-                {
-                    var item = Line.Substring(3).Trim();
-                    var COLOR = item.Substring(0, 6);
-                    if (ColorUtility.TryParseHtmlString(COLOR, out var c))
-                    {
-                        t.color = c;
-                    }
-                    item = item.Substring(7).Trim();
-                    SequentialProcessLine(item, t);
-                }
-                else
-                {
-                    t.text = Line;
-                }
+                t.fontSize = BaseSize;
+                t.color = DefaultColor;
+                SequentialProcessLine(item, t,BaseSize);
             }
         }
     }
