4bb9babb59549fdcc17798adc40227df385bb835
Creeper Lv <creeperlv@users.noreply.github.com>
2022-08-25 03:11:22 +0800

    [0.2.90.0]
    -Extended the animation of Alternation.

---
+++ Tools/CommonTools/Site13Kernel.UnityEngine/Data/LocalizationLoader.cs
@@ -0,0 +1,40 @@
+﻿using CLUNL.Localization;
+using Site13Kernel.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Site13Kernel.Data
+{
+    public class LocalizationLoader:ControlledBehavior
+    {
+        public bool LoadOnStartInsteadOfInit = false;
+        public bool ClearBeforeLoad = false;
+        public KVList<string, TextAsset> LanguageFiles = new KVList<string, TextAsset>();
+        public void Start()
+        {
+            if (LoadOnStartInsteadOfInit) Load();
+        }
+        public override void Init()
+        {
+            if (!LoadOnStartInsteadOfInit) Load();
+        }
+        void Load()
+        {
+
+            var Lan = Settings.CurrentSettings.LanguageCode.ToUpper();
+            TextAsset ToLoad = LanguageFiles.PrefabDefinitions[0].Value;
+            foreach (var item in LanguageFiles.PrefabDefinitions)
+            {
+                if (item.Key.ToUpper() == Lan)
+                {
+                    ToLoad = item.Value;
+                    break;
+                }
+            }
+            if (ClearBeforeLoad) Language.ClearLoadedStrings();
+            Language.LoadFromString(ToLoad.text);
+        }
+    }
+}
