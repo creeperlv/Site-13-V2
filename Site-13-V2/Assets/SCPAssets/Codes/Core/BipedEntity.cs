850086a83181284dfcd0ea50f63c3ce93f24fcd6
Creeper Lv <creeperlv@users.noreply.github.com>
2022-10-11 00:10:29 +0800

    [0.2.90.0]
    -Working on new weapon that will fit all (almost) weapon situation.

--- Site-13-V2/Assets/SCPAssets/Codes/Core/BipedEntity.cs
+++ Site-13-V2/Assets/SCPAssets/Codes/Core/BipedEntity.cs
@@ -4,6 +4,7 @@ namespace Site13Kernel.Core
 {
     public class BipedEntity : BioEntity
     {
+        public bool isPlayer = false;
         public Bag EntityBag;
     //    public override 
     }
--- Site-13-V2/Assets/SCPAssets/Codes/GameLogic/FPS/GenericWeapon.cs
+++ Site-13-V2/Assets/SCPAssets/Codes/GameLogic/FPS/GenericWeapon.cs
@@ -1,18 +1,23 @@
-﻿using Site13Kernel.Core;
+﻿using Site13Kernel.Animations;
+using Site13Kernel.Core;
 using Site13Kernel.Data;
+using Site13Kernel.GameLogic.Effects;
+using Site13Kernel.GameLogic.Level;
+using Site13Kernel.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
+using static FullScreenPassRendererFeature;
 
 namespace Site13Kernel.GameLogic.FPS
 {
     [Serializable]
-    public class GenericWeapon:ControlledBehavior
+    public class GenericWeapon : ControlledBehavior
     {
         public Weapon WeaponData;
-        public Animator WeaponAnimation;
+        public WrappedAnimator WeaponAnimation;
         public string Trigger_Idle;
         public string Trigger_Fire;
         public string Trigger_Overheat;
@@ -24,23 +29,62 @@ namespace Site13Kernel.GameLogic.FPS
         {
             None, TwoDig, ThreeDig, Liner, Text
         }
+        public bool isHoldByPlayer = false;
+        /// <summary>
+        /// 0 - Normal
+        /// 1 - Aim
+        /// </summary>
+        public int AimingMode = 0;
+        public float MaxHitScanDistance;
+        public WeaponFireType FireType = WeaponFireType.FullAuto;
+        public BulletFireType BulletFireType = BulletFireType.HitScan;
         [Header("Bullet")]
         public PrefabReference Bullet;
-        public PrefabReference FireEffect;
+        public PrefabReference EffectPrefab;
+        public PrefabReference BulletHitEffect;
+        public PrefabReference EjectionPrefab;
         [Header("Effects")]
         public Transform FirePoint;
+        public Transform CurrentFirePoint;
         public Transform EffectPoint;
-        public AudioSource FireSFX;
-        public List<AudioClip> FireSounds;
+        public Transform CurrentEffectPoint;
+        public Transform EjectionPoint;
+        public Transform ActualHolder;
+        public int FireSoundSetID;
+        public float EjectionForce = 1;
+        public List<AudioSource> GunSFXSources;
+        [Header("Pickupable Definition")]
+        public Pickupable Pickup;
+        public List<BoxCollider> AttachedColliders;
         [Header("Effects")]
         public float FireInterval;
-
+        public float Recoil = 0;
+        public float SingleFireRecoil = 0.5f;
+        public float MaxRecoil = 2;
+        public float RecoilRecoverSpeed = 5;
+        public float MaxScatterAngle = 0;
+        public float MaxScatterAngleAimMode = 0;
+        public bool ControlledBehaviorWorkflow;
+        [Header("Camera Shake")]
+        public float BaseCameraShakeIntensity = 0.3f;
+        public float AimModeMultiplier = 0.5f;
+        public float CamShakeDecay = 5f;
+        public float CamShakeSpeed = 50f;
+        public float CameraShakeIntensity = 0.3f;
+        public bool CreateBullet = true;
         public Site13Event OnSingleFire = new Site13Event();
+        public Site13Event OnRealProjetileFire = new Site13Event();
         public Site13Event OnOverheat = new Site13Event();
+        public Site13Event OnHit = new Site13Event();
         bool FIRE0;
         int FIRE1;
         bool FIRE2;
         bool FIRE3;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="WeaponConstants"/>
+        public int WeaponMode = WeaponConstants.WEAPON_MODE_NORMAL;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Fire()
@@ -51,6 +95,14 @@ namespace Site13Kernel.GameLogic.FPS
         public void Press()
         {
             FIRE3 = true;
+        }
+        public void Zoom()
+        {
+
+        }
+        public void Unzoom()
+        {
+
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Unfire()
@@ -66,7 +118,189 @@ namespace Site13Kernel.GameLogic.FPS
         }
         public void SingleFire()
         {
+            OnSingleFire.Invoke();
 
+            if ((WeaponData.CurrentMagazine > 0 && WeaponData.CurrentHeat < WeaponData.MaxHeat) || isHoldByPlayer == false)
+            {
+                if (FireType == WeaponFireType.FullAuto || FireType == WeaponFireType.SemiAuto)
+                {
+
+                    if (isHoldByPlayer)
+                    {
+                        this.WeaponData.CurrentMagazine--;
+                        NotifyWeaponAmmo();
+                        ActualHolder.GetComponentInChildren<CameraShakeEffect>().SetShake(1f, true, CamShakeDecay, true, CamShakeSpeed, CameraShakeIntensity, CameraShakeIntensity);
+                    }
+                    //if (OnCurrentMagChanged != null)
+                    //{
+                    //    OnCurrentMagChanged(WeaponData.CurrentMagazine);
+                    //}
+                    Quaternion Rotation = this.transform.rotation;
+                    Vector3 RecoilAngle = MathUtilities.RandomDirectionAngleOnXYAndZ0(Recoil / MaxRecoil * (AimingMode == 0 ? MaxScatterAngle : MaxScatterAngleAimMode), Camera.main.fieldOfView);
+                    Vector3 RecoilAngle2 = MathUtilities.RandomDirectionAngleOnXYAndZ1(Recoil / MaxRecoil * (AimingMode == 0 ? MaxScatterAngle : MaxScatterAngleAimMode));
+                    //Debug.Log(RecoilAngle);
+
+                    if (WeaponAnimation != null)
+                        WeaponAnimation.SetTrigger("Fire");
+                    WeaponData.CurrentHeat += WeaponData.HeatPerShot;
+                    if (WeaponData.CurrentHeat > WeaponData.MaxHeat)
+                    {
+                        WeaponMode = WeaponConstants.WEAPON_MODE_OVERHEAT;
+
+                        if (WeaponAnimation != null)
+                            WeaponAnimation.SetTrigger("Overheat");
+                    }
+                    {
+                        Vector3 V = Rotation.eulerAngles;
+                        V += RecoilAngle;
+                        Rotation = Quaternion.Euler(V);
+                    }
+                    if (EjectionPoint != null)
+                    {
+                        var GO = GameRuntime.CurrentGlobals.CurrentEffectController.Spawn(EjectionPrefab, EjectionPoint.position, EjectionPoint.rotation, Vector3.one);
+                        //GO.layer = this.gameObject.layer;
+                        {
+                            var RIG = GO.GetComponentInChildren<Rigidbody>();
+                            if (RIG != null)
+                            {
+                                RIG.AddForce(EjectionPoint.forward * EjectionForce, ForceMode.Impulse);
+                            }
+                        }
+                    }
+                    if (EffectPrefab != null)
+                    {
+                        var GO = GameRuntime.CurrentGlobals.CurrentEffectController.Spawn(EffectPrefab, CurrentEffectPoint.position, this.transform.rotation, Vector3.one, CurrentEffectPoint);
+                        GO.layer = this.gameObject.layer;
+                    }
+                    if (BulletFireType == BulletFireType.HitScan)
+                    {
+                        Vector3 _Rotation = FirePoint.forward;
+                        {
+                            _Rotation += FirePoint.TransformDirection(RecoilAngle2);
+                        }
+                        RaycastHit info;
+                        if (!isHoldByPlayer)
+                            Physics.Raycast(FirePoint.position, _Rotation, out info, MaxHitScanDistance, GameRuntime.CurrentGlobals.LayerExcludeAirBlock, QueryTriggerInteraction.Ignore);
+                        else Physics.Raycast(FirePoint.position, _Rotation, out info, MaxHitScanDistance, GameRuntime.CurrentGlobals.LayerExcludePlayerAndAirBlock, QueryTriggerInteraction.Ignore);
+                        if (info.collider != null)
+                        {
+                            if (info.collider.attachedRigidbody != null)
+                            {
+                                info.collider.attachedRigidbody.AddForce(_Rotation.normalized * WeaponData.PhysicsForce, ForceMode.Impulse);
+
+                            }
+                            //if (useEffectPointInsteadFirePoint)
+                            //{
+                            //    var _d = (info.point - EffectPoint.position).normalized;
+                            //    Rotation = Quaternion.LookRotation(_d);
+                            //}
+                            {
+                                var Hittable = info.collider.GetComponent<IHittable>();
+
+                                Quaternion quaternion = Quaternion.FromToRotation(Vector3.up, info.normal);
+                                if (Hittable != null)
+                                {
+                                    var f = GameRuntime.CurrentGlobals.CurrentEffectController.Spawn(Hittable.HitEffectHashCode(), info.point, quaternion, Vector3.one, info.collider.transform, true);
+                                    f.transform.localScale = new Vector3(1 / f.transform.lossyScale.x, 1 / f.transform.lossyScale.y, 1 / f.transform.lossyScale.z);
+                                }
+                                else
+                                {
+                                }
+                                if (BulletHitEffect != null)
+                                {
+                                    var f = GameRuntime.CurrentGlobals.CurrentEffectController.Spawn(BulletHitEffect, info.point, quaternion, Vector3.one);
+                                    f.transform.parent = info.collider.transform;
+                                    //f.transform.localScale = new Vector3(1f / f.transform.lossyScale.x, 1f / f.transform.lossyScale.y, 1f / f.transform.lossyScale.z);
+                                }
+                                var Entity = info.collider.GetComponent<DamagableEntity>();
+                                var WeakPoint = info.collider.GetComponent<WeakPoint>();
+                                if (WeakPoint != null)
+                                {
+                                    OnHit.Invoke();
+                                    WeakPoint.AttachedBioEntity.Damage(Bullet.GetPrefab().GetComponent<BaseBullet>().WeakPointDamage);
+                                }
+                                else if (Entity != null)
+                                {
+                                    CauseDamage(Entity);
+                                }
+                                else
+                                {
+                                    var Ref = info.collider.GetComponent<DamagableEntityReference>();
+                                    if (Ref != null)
+                                    {
+                                        Entity = Ref.Reference;
+                                        CauseDamage(Entity);
+                                    }
+                                }
+
+                            }
+                        }
+
+                        if (Bullet != null)
+                        {
+                            if (CreateBullet)
+                            {
+                                //if (!useEffectPointInsteadFirePoint)
+                                //    GameRuntime.CurrentGlobals.CurrentBulletSystem.AddBullet(BulletPrefab, FirePoint.position, Rotation, ActualHolder);
+                                //else
+                                {
+
+                                    GameRuntime.CurrentGlobals.CurrentBulletSystem.AddBullet(Bullet, CurrentEffectPoint.position, Rotation, ActualHolder.gameObject);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+
+                        if (Bullet != null)
+                        {
+                            //if (!useEffectPointInsteadFirePoint)
+                            //    GameRuntime.CurrentGlobals.CurrentBulletSystem.AddBullet(BulletPrefab, FirePoint.position, Rotation, ActualHolder);
+                            //else
+                            {
+                                GameRuntime.CurrentGlobals.CurrentBulletSystem.AddBullet(Bullet, EffectPoint.position, Rotation, ActualHolder.gameObject);
+                            }
+                        }
+                    }
+                    Recoil = Math.Min(Recoil + SingleFireRecoil, MaxRecoil);
+                    if (GunSFXSources.Count > 0)
+                    {
+                        var source = ListOperations.ObtainOne(GunSFXSources);
+                        source.Stop();
+                        source.clip = SoundResources.ObtainOneClip(FireSoundSetID);
+                        source.Play();
+                    }
+                }
+            }
+            else
+            {
+            }
+        }
+        private void CauseDamage(DamagableEntity Entity)
+        {
+            if (OnHit != null)
+            {
+                OnHit.Invoke();
+            }
+            Entity.Damage(Bullet.GetPrefab().GetComponent<BaseBullet>().BaseDamage);
+        }
+        void __frame(float DeltaT)
+        {
+
+        }
+        private void Update()
+        {
+            if (ControlledBehaviorWorkflow) return;
+            float DT = Time.deltaTime;
+            __frame(DT);
+        }
+        public override void Refresh(float DeltaTime, float UnscaledDeltaTime)
+        {
+            if (ControlledBehaviorWorkflow)
+            {
+                __frame(DeltaTime);
+            }
         }
         public void NotifyWeaponAmmo()
         {
