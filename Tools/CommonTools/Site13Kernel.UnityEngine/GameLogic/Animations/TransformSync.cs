4ed07cdf8a0e06f357e4e79579ffba184bc9136b
Creeper Lv <creeperlv@users.noreply.github.com>
2023-06-29 01:29:59 +0800

    [0.2.90.0] -Added a TransformSync.

---
+++ Tools/CommonTools/Site13Kernel.UnityEngine/GameLogic/Animations/TransformSync.cs
@@ -0,0 +1,27 @@
+﻿using Site13Kernel.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Site13Kernel.GameLogic.Animations
+{
+    public class TransformSync : ControlledBehavior
+    {
+        public List<TrackedTransforms> transforms = new List<TrackedTransforms>();
+        public void Update()
+        {
+            foreach (var item in transforms)
+            {
+                item.Target.position = item.Source.position;
+                item.Target.rotation = item.Source.rotation;
+            }
+        }
+    }
+    [Serializable]
+    public class TrackedTransforms
+    {
+        public Transform Source;
+        public Transform Target;
+    }
+}
