22bcd1eb4819a5eeb398a41c0c445585c13b0044
Creeper Lv <creeperlv@users.noreply.github.com>
2023-06-30 00:53:04 +0800

    [0.2.90.0] -Made a custom editor to generate a biped ready transform tree. -TransformSync should be working now.

--- Tools/CommonTools/Site13Kernel.UnityEngine/GameLogic/Animations/TransformSync.cs
+++ Tools/CommonTools/Site13Kernel.UnityEngine/GameLogic/Animations/TransformSync.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using UnityEngine;
 
 namespace Site13Kernel.GameLogic.Animations
@@ -9,12 +10,22 @@ namespace Site13Kernel.GameLogic.Animations
     public class TransformSync : ControlledBehavior
     {
         public List<TrackedTransforms> transforms = new List<TrackedTransforms>();
+        public void Start()
+        {
+            foreach (var transform in transforms)
+            {
+                transform.PositionDelta = transform.Target.localPosition - transform.Source.localPosition;
+                //transform.RotationDelta = Quaternion.Inverse(transform.Source.localRotation)*transform.Target.localRotation;
+                transform.RotationDelta = (Quaternion.Euler(transform.Target.eulerAngles - transform.Source.eulerAngles));
+            }
+        }
+        
         public void Update()
         {
             foreach (var item in transforms)
             {
-                item.Target.position = item.Source.position;
-                item.Target.rotation = item.Source.rotation;
+                item.Target.localPosition = item.Source.localPosition + item.PositionDelta;
+                item.Target.rotation= item.Source.rotation*item.RotationDelta ;
             }
         }
     }
@@ -23,5 +34,7 @@ namespace Site13Kernel.GameLogic.Animations
     {
         public Transform Source;
         public Transform Target;
+        public Vector3 PositionDelta;
+        public Quaternion RotationDelta;
     }
 }
