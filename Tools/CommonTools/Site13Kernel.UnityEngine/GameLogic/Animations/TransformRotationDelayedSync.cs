fdb490af6b5cc1fb9bcf39d005d4320af5a8d984
Creeper Lv <creeperlv@users.noreply.github.com>
2023-07-04 01:32:30 +0800

    [0.2.90.0] -Added Rotation Delayed Sync for bipeds.

---
+++ Tools/CommonTools/Site13Kernel.UnityEngine/GameLogic/Animations/TransformRotationDelayedSync.cs
@@ -0,0 +1,86 @@
+﻿using Site13Kernel.Core;
+using Site13Kernel.Utilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Site13Kernel.GameLogic.Animations
+{
+	public class TransformRotationDelayedSync : ControlledBehavior
+	{
+		public Transform Source;
+		public Transform Target;
+		public float Tolerance = 15;
+		public float Speed = 10;
+		public bool WillTriggerAnimation;
+		public Animator TargetAnimator;
+		public List<string> AnimationTrigger;
+		public bool IsLocalSpace = true;
+		public bool IsMoving;
+		public float Termination = 2;
+		public void Sync()
+		{
+
+			Quaternion Src;
+			Quaternion Tgt;
+			if (IsLocalSpace)
+			{
+				Src = Source.localRotation;
+				Tgt = Target.localRotation;
+				if (Src != Tgt) Target.localRotation = Src;
+			}
+			else
+			{
+
+				Src = Source.rotation;
+				Tgt = Target.rotation;
+				if (Src != Tgt) Target.rotation = Src;
+			}
+		}
+		public void Update()
+		{
+			float angle;
+			Quaternion Src;
+			Quaternion Tgt;
+			if (IsLocalSpace)
+			{
+				Src = Source.localRotation;
+				Tgt = Target.localRotation;
+			}
+			else
+			{
+
+				Src = Source.rotation;
+				Tgt = Target.rotation;
+			}
+			angle = Quaternion.Angle(Src , Tgt);
+			if (IsMoving)
+			{
+				if (angle <= Termination)
+				{
+					IsMoving = false;
+					return;
+				}
+				var f_q = Quaternion.RotateTowards(Tgt , Src , angle * Speed * Time.deltaTime);
+				if (IsLocalSpace)
+				{
+					Target.localRotation = f_q;
+				}
+				else Target.rotation = f_q;
+			}
+			else
+			{
+				if (angle >= Tolerance)
+				{
+					IsMoving = true;
+				}
+				if (WillTriggerAnimation)
+				{
+					if (TargetAnimator != null)
+					{
+						TargetAnimator.SetTrigger(AnimationTrigger [ RandomTool.NextInt(AnimationTrigger.Count) ]);
+					}
+				}
+			}
+		}
+	}
+}
--- Tools/CommonTools/Site13Kernel.UnityEngine/GameLogic/Animations/TransformSync.cs
+++ Tools/CommonTools/Site13Kernel.UnityEngine/GameLogic/Animations/TransformSync.cs
@@ -7,34 +7,34 @@ using UnityEngine;
 
 namespace Site13Kernel.GameLogic.Animations
 {
-    public class TransformSync : ControlledBehavior
-    {
-        public List<TrackedTransforms> transforms = new List<TrackedTransforms>();
-        public void Start()
-        {
-            foreach (var transform in transforms)
-            {
-                transform.PositionDelta = transform.Target.localPosition - transform.Source.localPosition;
-                //transform.RotationDelta = Quaternion.Inverse(transform.Source.localRotation)*transform.Target.localRotation;
-                transform.RotationDelta = (Quaternion.Euler(transform.Target.eulerAngles - transform.Source.eulerAngles));
-            }
-        }
-        
-        public void Update()
-        {
-            foreach (var item in transforms)
-            {
-                item.Target.localPosition = item.Source.localPosition + item.PositionDelta;
-                item.Target.rotation= item.Source.rotation*item.RotationDelta ;
-            }
-        }
-    }
-    [Serializable]
-    public class TrackedTransforms
-    {
-        public Transform Source;
-        public Transform Target;
-        public Vector3 PositionDelta;
-        public Quaternion RotationDelta;
-    }
+	public class TransformSync : ControlledBehavior
+	{
+		public List<TrackedTransforms> transforms = new List<TrackedTransforms>();
+		public void Start()
+		{
+			foreach (var transform in transforms)
+			{
+				transform.PositionDelta = transform.Target.localPosition - transform.Source.localPosition;
+				//transform.RotationDelta = Quaternion.Inverse(transform.Source.localRotation)*transform.Target.localRotation;
+				transform.RotationDelta = (Quaternion.Euler(transform.Target.eulerAngles - transform.Source.eulerAngles));
+			}
+		}
+
+		public void Update()
+		{
+			foreach (var item in transforms)
+			{
+				item.Target.localPosition = item.Source.localPosition + item.PositionDelta;
+				item.Target.rotation = item.Source.rotation * item.RotationDelta;
+			}
+		}
+	}
+	[Serializable]
+	public class TrackedTransforms
+	{
+		public Transform Source;
+		public Transform Target;
+		public Vector3 PositionDelta;
+		public Quaternion RotationDelta;
+	}
 }
